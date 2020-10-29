47b7065d8181e5c77350a87be140c5e5d39a6123
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-29 19:52:29 -0300

    app routes

---
+++ src/routes/app/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import AuthRoutes from './auth';
+import AppRoutes from './app';
+
+const Routes = () => {
+
+  const signed = false;
+
+  return signed ? <AppRoutes /> : <AuthRoutes />;
+}
+
+export default Routes;
