35b4b26e3e50473382d6009d72c6fe6bbaa1c838
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-29 19:50:12 -0300

    start screens and routes

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import PresentationRoutes from './presentation';
+import AppRoutes from './app';
+
+const Routes = () => {
+
+  const firstAccess = true;
+
+  return (
+    <NavigationContainer>
+      {firstAccess ? <PresentationRoutes /> : <AppRoutes />}
+    </NavigationContainer>
+  )
+}
+
+export default Routes;
---
+++ src/screens/Presentation/Donations/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const Donations = () => {
+  return <Text>Donations</Text>;
+}
+
+export default Donations;
