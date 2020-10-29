1835aec31e2e9bf5d40445425b24c607b4be9c48
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-29 20:20:07 -0300

    bottom tabs style

--- src/routes/app/app.js
+++ src/routes/app/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo } from '@expo/vector-icons';
 
 import Dashboard from '../../screens/App/Dashboard';
 
@@ -7,8 +8,40 @@ const Tab = createBottomTabNavigator();
 
 const AppRoutes = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Dashboard" component={Dashboard} />
+    <Tab.Navigator tabBarOptions={{
+      style: {
+        elevation: 0,
+        shadowOpacity: 0,
+        height: 64,
+      },
+      tabStyle: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      iconStyle: {
+        flex: 0,
+        width: 20,
+        height: 20,
+      },
+      labelStyle: {
+        fontSize: 14,
+      },
+      inactiveTintColor: '#999999',
+      activeTintColor: '#FF6B6C',
+    }}>
+      <Tab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarLabel: 'Inicio',
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Entypo name="home" size={size} color={focused ? color : '#999'} />
+            );
+          }
+        }}
+      />
     </Tab.Navigator>
   );
 }
