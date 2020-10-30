9567e6f1e377c4c12fd81e18b82c11d8ed27cf05
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-30 19:05:29 -0300

    get async storage data

--- src/hooks/app.js
+++ src/hooks/app.js
@@ -4,13 +4,9 @@ import AsyncStorage from '@react-native-community/async-storage';
 const AppContext = createContext({});
 
 const AppProvider = ({ children }) => {
-  const [firstAccess, setFirstAccess] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      await AsyncStorage.getItem('@helper:firstAccess');
-    })();
-  }, []);
+  const [firstAccess, setFirstAccess] = useState(
+    async () => await AsyncStorage.getItem('@helper:firstAccess') === null && true
+  );
 
   async function presentationDone() {
     await AsyncStorage.setItem('@helper:firstAccess', false);
