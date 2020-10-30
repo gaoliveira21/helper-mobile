5f2be76dd4ef13603a684d5d9023f1bec63ab1fc
Gabriel <gaoliveira211999@gmail.com>
2020-10-30 14:44:08 -0300

    async storage

--- src/hooks/app.js
+++ src/hooks/app.js
@@ -1,14 +1,27 @@
-import React, { createContext, useEffect, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const AppContext = createContext({});
 
 const AppProvider = ({ children }) => {
-  const [firstAccess, setFirstAccess] = useState(true);
+  const [firstAccess, setFirstAccess] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      await AsyncStorage.getItem('@helper:firstAccess');
+    })();
+  }, []);
+
+  async function presentationDone() {
+    await AsyncStorage.setItem('@helper:firstAccess', false);
+    setFirstAccess(false);
+  }
 
   return (
     <AppContext.Provider
       value={{
-        firstAccess
+        firstAccess,
+        presentationDone
       }}
     >
       {children}
