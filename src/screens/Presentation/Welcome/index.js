f5ba17e78fba46f00233324f770d2d58fa17d2a8
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-29 21:19:48 -0300

    simple welcome styles

--- src/screens/Presentation/Welcome/index.js
+++ src/screens/Presentation/Welcome/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container, LogoText } from './styles';
 
 const Welcome = () => {
-  return <View />;
+  return (
+    <Container>
+      <LogoText>Helper</LogoText>
+    </Container>
+  );
 }
 
 export default Welcome;
---
+++ src/screens/Presentation/Welcome/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  width: 100%;
+  padding: 20px;
+  background: #F9F9F9;
+`;
+
+export const LogoText = styled.Text`
+  font-family: 'Roboto';
+  color: #FF6B6C;
+  font-size: 56px;
+  font-weight: bold;
+`;
