8a4ef9ecaa2197650a71c6b165f270c0f85a34e4
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-31 17:01:22 -0300

    profile stack navigation

---
+++ src/screens/App/Profile/ChangePassword/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const ChangePassword = () => {
+  return <View />;
+}
+
+export default ChangePassword;
