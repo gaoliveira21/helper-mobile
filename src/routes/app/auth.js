9cef55d27771ee289255ba15ddfa93729dcce966
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-11-01 14:23:27 -0300

    sign up step 1 header styles

--- src/routes/app/auth.js
+++ src/routes/app/auth.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '../../screens/App/Auth/SignIn';
+import Step1 from '../../screens/App/Auth/SignUp/Step1';
+import Step2 from '../../screens/App/Auth/SignUp/Step2';
 
 const Stack = createStackNavigator();
 
 const AuthRoutes = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }}/>
+    <Stack.Navigator
+      screenOptions={{
+        headerTransparent: true,
+        headerTintColor: '#FF6B6C',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 20,
+        },
+        headerLeftContainerStyle: {
+          marginLeft: 20,
+        },
+      }}
+    >
+      <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
+      <Stack.Screen name="SignUpStep1" component={Step1} options={({ navigation }) => ({
+        title: 'Cadastro',
+        headerTitleAlign: 'center',
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
+            <Entypo name="chevron-left" size={28} color="#FF6B6C" />
+          </TouchableOpacity>
+        )
+      })} />
+      <Stack.Screen name="SignUpStep2" component={Step2} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 }
