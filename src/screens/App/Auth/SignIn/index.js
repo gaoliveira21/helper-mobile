e8062cbca5f73f7c6beb2792ad95409a3168dd61
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-11-01 13:44:03 -0300

    SignIn styles

--- src/screens/App/Auth/SignIn/index.js
+++ src/screens/App/Auth/SignIn/index.js
@@ -1,10 +1,56 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Title,
+  Form,
+  InputContent,
+  Label,
+  Input,
+  Field,
+  Error,
+  SubmitButton,
+  SignUpButton,
+  ForgotPasswordText,
+  ClickHere,
+  ButtonText
+} from './styles';
 
 const SignIn = () => {
-  return <Text>SignIn</Text>;
+  return (
+    <Container>
+      <Title>Login</Title>
+      <Form>
+        <InputContent>
+          <Label>Email</Label>
+          <Input>
+            <Field placeholder="example@mail.com" placeholderTextColor="#999999" />
+            <MaterialIcons name="email" size={24} color="#999999" />
+          </Input>
+          <Error>Error message example</Error>
+        </InputContent>
+
+        <InputContent>
+          <Label>Senha</Label>
+          <Input>
+            <Field placeholder="***********" placeholderTextColor="#999999" />
+            <MaterialIcons name="remove-red-eye" size={24} color="#999999" />
+          </Input>
+          <Error>Error message example</Error>
+        </InputContent>
+        <SubmitButton>
+          <ButtonText>Entrar</ButtonText>
+        </SubmitButton>
+        <SignUpButton>
+          <ButtonText>Cadastrar-se</ButtonText>
+        </SignUpButton>
+      </Form>
+      <ForgotPasswordText>
+        Esqueceu sua senha? <ClickHere>Clique aqui</ClickHere>
+      </ForgotPasswordText>
+    </Container>
+  );
 }
 
 export default SignIn;
