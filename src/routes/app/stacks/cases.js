9e054707fd40b37f0bb5c4f1523ac213e3a8718c
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-11-02 12:53:08 -0300

    fix routes proptypes

--- src/routes/app/stacks/cases.js
+++ src/routes/app/stacks/cases.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-shadow */
 import React, { useLayoutEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
