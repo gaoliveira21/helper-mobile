a6717e746ac900c20eeba0f433606295a24346b9
Pedro <pedro.lucas210600@gmail.com>
2020-11-02 14:22:17 -0300

    Update password and success screen

--- src/screens/App/Profile/ChangePassword/index.js
+++ src/screens/App/Profile/ChangePassword/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-// import { Container } from './styles';
+import { MaterialIcons } from '@expo/vector-icons';
+
+import Input from '../../../../components/Input';
+
+import { Form, Button, TextButton } from './styles';
 
 const ChangePassword = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  return (
+    <Form>
+      <Input
+        label="Senha atual"
+        placeholder="Insira sua senha atual"
+        icon={({ color }) => (
+          <MaterialIcons name="remove-red-eye" size={24} color={color} />
+        )}
+        error=""
+      />
+      <Input
+        label="Nova senha"
+        placeholder="Insira sua nova senha"
+        icon={({ color }) => (
+          <MaterialIcons name="remove-red-eye" size={24} color={color} />
+        )}
+        error=""
+      />
+      <Input
+        label="Confirmar nova senha"
+        placeholder="Confirme sua nova senha"
+        icon={({ color }) => (
+          <MaterialIcons name="remove-red-eye" size={24} color={color} />
+        )}
+        error=""
+      />
+
+      <Button color="#FF6B6C" onPress={() => navigation.navigate('Success')}>
+        <MaterialIcons name="lock" color="#FFF" size={24} />
+        <TextButton>Alterar Senha</TextButton>
+      </Button>
+    </Form>
+  );
 };
 
 export default ChangePassword;
--- src/screens/App/Profile/Success/index.js
+++ src/screens/App/Profile/Success/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-// import { Container } from './styles';
+import { Container, Title, AccessButton, AccessButtonText } from './styles';
 
 const Success = () => {
-  return <View />;
+  return (
+    <Container>
+      <Title>Cadastro realizado com sucessso!</Title>
+      <Feather name="check-circle" size={156} color="#6FCF97" />
+      <AccessButton>
+        <AccessButtonText>Acessar o app</AccessButtonText>
+      </AccessButton>
+    </Container>
+  );
 };
 
 export default Success;
